6f9b83e751a468b3e2b93d89be8489cfde6650f3
geno nullfree <nullfree.geno@gmail.com>
2026-05-25 16:12:21 -0400

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ struct Account {
 
 impl Account {
     pub fn new_user(&mut self, name: &str, deposit: f64) {
-        let user = User::new(name, deposit);
+        let mut user = User::new(name, deposit);
+        user.deposit(self.total, deposit);
         self.users.push(user);
         self.update();
     }
@@ -22,8 +23,8 @@ impl Account {
         }
 
         for each in &mut self.users {
-            let p = each.deposit / total;
-            each.percent = Some(p);
+            let p = (each.deposit - each.withdrawl) / total;
+            each.percent = p;
         }
 
         self.total = total + self.investment - withdrawl;
@@ -41,7 +42,7 @@ impl Account {
 
         let mut check = 0f64;
         for each in &self.users {
-            let p = each.percent.unwrap_or_default();
+            let p = each.percent;
             println!("Account: {}", each.name);
             println!("Account Percent: {:.02?}%", p * 100f64);
             println!("Account Balance: ${:.02}", (self.total * p) - each.withdrawl + (total_withdrawl * p));
@@ -73,6 +74,10 @@ impl Account {
     pub fn withdrawl(&mut self, name: &str, value: f64) {
         for each in &mut self.users {
             if each.name == name {
+                if each.withdrawl + value > each.deposit {
+                    println!("Withdrawl failed! Not enough funds in {name}'s account.");
+                    return;
+                }
                 each.withdrawl += value;
                 println!("Modified Account: {name}");
                 println!("Total withdrawl: {:.02}", each.withdrawl);
@@ -90,7 +95,7 @@ struct User {
     name: String,
     deposit: f64,
     withdrawl: f64,
-    percent: Option<f64>,
+    percent: f64,
 }
 
 impl User {
@@ -99,26 +104,50 @@ impl User {
             name: name.to_string(),
             deposit,
             withdrawl: 0f64,
-            percent: None,
+            percent: 0f64,
         }
+    }
+
+    pub fn deposit(&mut self, total: f64, deposit: f64) {
+        let value = self.value(total);
+        self.percent = (value + deposit) / (total + deposit);
+    }
+
+    pub fn withdrawl(&mut self, total: f64, withdrawl: f64) -> bool {
+        let value = self.value(total);
+        if value > withdrawl {
+            self.percent = (value - withdrawl) / (total - withdrawl);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn value(&self, total: f64) -> f64 {
+        total * self.percent
     }
 }
 
 fn main() {
     let mut acct = Account{ total: 5000f64, investment: 0f64, users: Vec::new()};
     
-    let user0 = User::new("geno", 52000f64);
-    let user1 = User::new("Micah", 1300f64);
-    let user2 = User::new("Killian", 1000f64);
-    let user3 = User::new("Ashton", 500f64);
-    let user4 = User::new("Areli", 300f64);
+    acct.new_user("geno", 52000f64);
+    acct.totals();
+    acct.new_user("Micah", 1300f64);
+    acct.totals();
+    acct.new_user("Killian", 1000f64);
+    acct.totals();
+    acct.new_user("Ashton", 500f64);
+    acct.totals();
+    acct.new_user("Areli", 300f64);
+    acct.totals();
 
+    /*
     let users = vec![user0, user1, user2, user3, user4];
     acct.users = users;
     acct.update();
+    */
 
-    println!("Account: {acct:?}");
-    acct.totals();
     acct.investments(10000.00);
     acct.totals();
     acct.investments(-3000.00);
